27237f1769ae63b628ba0e89086c6251650f9a46
MDeitrick75 <MarkusDeitrick@gmail.com>
2025-01-26 15:39:15 -0800

    Kass's keep theme added

--- source/index.js
+++ source/index.js
@@ -14,9 +14,15 @@ const wizmusic = new Audio(require("music/Party Wizard.mp3"))
 wizmusic.loop = true;
 const quietmusic = new Audio(require("music/Deltador_s Domain.wav"))
 quietmusic.loop = true
+const kassmusic = new Audio(require("music/Kass's Keep.mp3"))
+kassmusic.loop = true
 
 const WIZ_POSITIONS = [[32, 10], [4, 18], [23, 28]]
 const WIZ_RADIUS = 5
+
+const CASTLE_ENTRANCES = [[32, 32], [32, 33], [32, 34], [41, 17]]
+const CASTLE_EXITS = [[31, 31], [31, 32], [31, 33], [31, 34], [31, 35], [40, 16], [41, 16], [40, 17], [29, 15]]
+var inCastle = false
 
 let loop = new Yaafloop(function(delta) {
     App.update(delta)
@@ -46,9 +52,31 @@ let loop = new Yaafloop(function(delta) {
             nearWiz = true
         }
     }
+
     if(!nearWiz) {
         wizmusic.volume = 0.0
         bigmusic.volume = 1.0
     }
 
+    // Kass's keep song
+    // Entrances
+    for(var i = 0; i < CASTLE_ENTRANCES.length; i++) {
+        if(App.player.position.x == CASTLE_ENTRANCES[i][0] && App.player.position.y == CASTLE_ENTRANCES[i][1]) {
+            inCastle = true
+            kassmusic.play()
+            break;
+        }
+    }
+    // Exits
+    for(var i = 0; i < CASTLE_EXITS.length; i++) {
+        if(App.player.position.x == CASTLE_EXITS[i][0] && App.player.position.y == CASTLE_EXITS[i][1]) {
+            inCastle = false
+            kassmusic.pause()
+            break;
+        }
+    }
+
+    if(inCastle) {
+        bigmusic.volume = 0.0
+    }
 })
